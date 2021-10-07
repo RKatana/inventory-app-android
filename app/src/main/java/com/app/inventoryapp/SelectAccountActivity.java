90efb1556214fd74249e7c692c0d30e2455932fa
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-10-07 20:26:11 +0300

    added activity intent to direct the button to intended activity

--- app/src/main/java/com/app/inventoryapp/SelectAccountActivity.java
+++ app/src/main/java/com/app/inventoryapp/SelectAccountActivity.java
@@ -2,16 +2,18 @@ package com.app.inventoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SelectAccountActivity extends AppCompatActivity {
-    @BindView(R.id.btnStoreAttendant) Button btnStoreAttendant;
-    @BindView(R.id.btnStoreOwner) Button btnStoreOwner;
+public class SelectAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.btnStoreAttendant) Button mBtnStoreAttendant;
+    @BindView(R.id.btnStoreOwner) Button mBtnStoreOwner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,23 @@ public class SelectAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_account);
 
         ButterKnife.bind(this);
+        mBtnStoreAttendant.setOnClickListener(this);
+        mBtnStoreOwner.setOnClickListener(this);
 
         
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mBtnStoreAttendant){
+            Intent intent = new Intent(SelectAccountActivity.this, AttendantLoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        if (view == mBtnStoreOwner){
+            Intent intent = new Intent(SelectAccountActivity.this, OwnerLoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }

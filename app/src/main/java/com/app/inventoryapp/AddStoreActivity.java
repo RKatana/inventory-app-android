f378ad16d823e2bc5d6e77110b0f30df802b7ccf
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-10-09 10:19:55 +0300

    Directed add store to add store admin

--- app/src/main/java/com/app/inventoryapp/AddStoreActivity.java
+++ app/src/main/java/com/app/inventoryapp/AddStoreActivity.java
@@ -2,13 +2,35 @@ package com.app.inventoryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddStoreActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddStoreActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.addStoreButton) Button mAddStoreButton;
+    @BindView(R.id.shopNameEditText) EditText mShopNameEditText;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_store);
+        ButterKnife.bind(this);
+
+        mAddStoreButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mAddStoreButton){
+            Intent intent = new Intent(AddStoreActivity.this, AddStoreAdminActivity.class);
+            intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
     }
 }

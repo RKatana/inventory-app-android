8b0d0b7f84fc4e08f3c4fe5db26ad093fb99c6b9
Steve-njeri <stevenjeri254@gmail.com>
2021-10-09 11:31:40 +0300

    Added new file StoresDashboardActivity and created adapter package file

--- app/src/main/java/com/app/inventoryapp/StoresDashboardActivity.java
+++ app/src/main/java/com/app/inventoryapp/StoresDashboardActivity.java
@@ -3,12 +3,16 @@ package com.app.inventoryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class StoresDashboardActivity extends AppCompatActivity {
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stores_dashboard);
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
     }
 }
---
+++ app/src/main/java/com/app/inventoryapp/adapter/StoreListAdapter.java
@@ -0,0 +1,4 @@
+package com.app.inventoryapp.adapter;
+
+public class StoreListAdapter {
+}

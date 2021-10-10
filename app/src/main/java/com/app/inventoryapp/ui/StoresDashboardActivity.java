2fe561259f5d4de57609a467928cd9baa48cf45a
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-10-10 12:24:16 +0300

    updated StoreDashboardActivity

--- app/src/main/java/com/app/inventoryapp/ui/StoresDashboardActivity.java
+++ app/src/main/java/com/app/inventoryapp/ui/StoresDashboardActivity.java
@@ -6,42 +6,51 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.app.inventoryapp.R;
+import com.app.inventoryapp.adapter.ProductsListAdapter;
 import com.app.inventoryapp.adapter.StoreListAdapter;
 import com.app.inventoryapp.models.Store;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class StoresDashboardActivity extends AppCompatActivity {
     private static final String TAG = "StoresDashboardActivity";
-    private RecyclerView storeRecyclerView;
+    @BindView(R.id.storeRecyclerView) RecyclerView mRecyclerView;
+
+    Store store1 = new Store("Store One","Nairobi Karen");
+    Store store2 = new Store("Store Two","Kenya");
+    Store store3 = new Store("Store Three","Karen");
+    Store store4 = new Store("Store Four"," Ngong");
+    Store store5 = new Store("Store Five","Samburu");
+    Store store6 = new Store("Store Six","Kitengela");
+    Store store7 = new Store("Store Seven","Don home");
+    Store store8 = new Store("Store Eight","Ngara");
+
+    private StoreListAdapter mAdapter;
+    public List<Store> Stores = Arrays.asList(new Store[]{store1, store2, store3, store4, store5, store6, store7, store8});
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stores_dashboard);
-        Log.d(TAG,"onCreate: Started");
 
-        storeRecyclerView = (RecyclerView) findViewById(R.id.storeRecyclerView);
-        StoreListAdapter adapter = new StoreListAdapter(this);
-        storeRecyclerView.setAdapter(adapter);
-        storeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        ButterKnife.bind(this);
 
-        ArrayList<Store> stores = new ArrayList<>();
+        mAdapter = new StoreListAdapter(Stores,this);
+        mRecyclerView.setAdapter(mAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(StoresDashboardActivity.this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setVisibility(View.VISIBLE);
 
-        stores.add(new Store("Store One","Nairobi Karen"));
-        stores.add(new Store("Store Two","Kenya"));
-        stores.add(new Store("Store Three","Karen"));
-        stores.add(new Store("Store Four","Samburu"));
-        stores.add(new Store("Store Five","Ngon"));
-        stores.add(new Store("Store Six","Karen"));
-        stores.add(new Store("Store Six","Karen"));
-        stores.add(new Store("Store Six","Karen"));
-        stores.add(new Store("Store Six","Karen"));
-        stores.add(new Store("Store Six","Karen"));
 
-        adapter.setStores(stores);
     }
 }

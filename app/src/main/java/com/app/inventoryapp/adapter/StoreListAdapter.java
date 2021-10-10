56c569a893f6b00ab7726f775e725f45b382cc85
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-10-10 12:23:39 +0300

    updated StoreListAdapter

--- app/src/main/java/com/app/inventoryapp/adapter/StoreListAdapter.java
+++ app/src/main/java/com/app/inventoryapp/adapter/StoreListAdapter.java
@@ -16,61 +16,67 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.app.inventoryapp.R;
 import com.app.inventoryapp.models.Store;
 import com.app.inventoryapp.ui.AttendantLoginActivity;
+import com.app.inventoryapp.ui.MainActivity;
+import com.app.inventoryapp.ui.ProductsActivity;
 import com.app.inventoryapp.ui.SelectAccountActivity;
 import com.app.inventoryapp.ui.StoresDashboardActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.ViewHolder>  {
-    private static final String TAG = "StoreListAdapter";
-    private ArrayList<Store> stores = new ArrayList<>();
-    private Context context;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    public StoreListAdapter(Context context) {
-        this.context = context;
+public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.StoreViewHolder>  {
+
+    private List<Store> mStores;
+    private  Context mContext;
+
+    public StoreListAdapter(List<Store> mStores, Context mContext) {
+        this.mStores = mStores;
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stores_list_item, null);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
+    public StoreListAdapter.StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stores_list_item,parent,false);
+        StoreViewHolder viewHolder = new StoreViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull  ViewHolder holder , int position) {
-        Log.d(TAG, "onBindViewHolder: called");
-        holder.storeName.setText(stores.get(position).getName());
-        holder.storeLocation.setText(stores.get(position).getLocation());
-        holder.storeName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+    public void onBindViewHolder(@NonNull StoreListAdapter.StoreViewHolder holder, int position) {
+        holder.bindStore(mStores.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return stores.size();
+        return mStores.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView storeName;
-        private TextView storeLocation;
+    public  class StoreViewHolder extends  RecyclerView.ViewHolder implements  View.OnClickListener{
+        @BindView(R.id.storeName) TextView mStoreName;
+        @BindView(R.id.storeLocation) TextView mStoreLocation;
 
-        public ViewHolder(@NonNull View itemView) {
+        public StoreViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            storeName = (TextView) itemView.findViewById(R.id.storeName);
-            storeLocation = (TextView) itemView.findViewById(R.id.storeLocation);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
         }
-    }
 
-    public void setStores(ArrayList<Store> stores) {
-        this.stores = stores;
-        notifyDataSetChanged();
+        public void bindStore (Store store){
+            mStoreName.setText(store.getName());
+            mStoreLocation.setText(store.getLocation());
+        }
+
+        @Override
+        public void onClick(View view) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, ProductsActivity.class);
+            intent.putExtra("position", itemPosition);
+            mContext.startActivity(intent);
+        }
     }
 }

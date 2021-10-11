14a11e8727ba74e89c81033653f0933f87764d01
Steve-njeri <stevenjeri254@gmail.com>
2021-10-11 03:25:27 +0300

    modified files

--- app/src/main/java/com/app/inventoryapp/ui/MainActivity.java
+++ app/src/main/java/com/app/inventoryapp/ui/MainActivity.java
@@ -1,11 +1,12 @@
 package com.app.inventoryapp.ui;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.app.inventoryapp.R;
 import com.app.inventoryapp.databinding.ActivityMainBinding;
@@ -19,6 +20,8 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -29,50 +32,58 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = "MainActivity";
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private Toolbar toolbar;
     private FrameLayout frameLayout;
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
+        initViews();
 
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container_view_tag, new StoreDetailsFragment());
+        transaction.commit();
+    }
+
+    private void initViews() {
+        Log.d(TAG,"initViews: selected");
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
     }
 
     @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Clerk:
+                Toast.makeText(this,"Clerks",Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.logout:
+                Toast.makeText(this,"logout",Toast.LENGTH_SHORT).show();
+
+                break;
+            default:
+                break;
         }
+        return false;
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
---
+++ app/src/main/java/com/app/inventoryapp/ui/StoreDetailsFragment.java
@@ -0,0 +1,66 @@
+package com.app.inventoryapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.app.inventoryapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class StoreDetailsFragment extends Fragment {
+
+    private static final String TAG = "StoreDetailsFragment ";
+    private BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_store_details, container, false);
+
+        initView(view);
+        initBottomNavigation();
+
+        return view;
+    }
+
+    private void initView(View view) {
+        Log.d(TAG,"initView: selected");
+    }
+
+    private void initBottomNavigation() {
+        Log.d(TAG, "initBottomNavigation: selected");
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.search:
+
+                        break;
+                    case R.id.home:
+
+                        break;
+                    case R.id.Clerk:
+                        Toast.makeText(getActivity(), "Clerk selected", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    default:
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+}

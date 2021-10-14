df7059fb23caba08f2ab4582aaff67493277325e
saitemu_issa <saitemu.issa@student.moringaschool.com>
2021-10-14 18:23:12 +0300

    made changes to clerks button to direct GraphReportActivity when  adding GraphReportActivity

--- app/src/main/java/com/app/inventoryapp/ui/StoreDetailsFragment.java
+++ app/src/main/java/com/app/inventoryapp/ui/StoreDetailsFragment.java
@@ -78,6 +78,8 @@ public class StoreDetailsFragment extends Fragment{
                 switch (item.getItemId()) {
                     case R.id.Profile:
                         Toast.makeText(getActivity(), "Profile selected", Toast.LENGTH_SHORT).show();
+                        Intent GraphIntent = new Intent(getActivity(), GraphReportActivity.class);
+                        startActivity(GraphIntent);
 
                         break;
                     case R.id.home:

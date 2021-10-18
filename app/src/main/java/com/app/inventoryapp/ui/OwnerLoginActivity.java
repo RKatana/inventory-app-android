32d0ab9774609955ccb17169f19c39b47c6f8aa5
Steve-njeri <stevenjeri254@gmail.com>
2021-10-18 23:39:47 +0300

    modified files

--- app/src/main/java/com/app/inventoryapp/ui/OwnerLoginActivity.java
+++ app/src/main/java/com/app/inventoryapp/ui/OwnerLoginActivity.java
@@ -33,6 +33,10 @@ public class OwnerLoginActivity extends AppCompatActivity implements View.OnClic
 
         mRegisterTextView.setOnClickListener(this);
         mPasswordLoginButton.setOnClickListener(this);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 
     @Override
